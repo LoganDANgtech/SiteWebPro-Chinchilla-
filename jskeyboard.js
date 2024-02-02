03f31f6f422fc4f4ad958363b038b9f28d18f088
LoganDANgtech <ldan@gaming.tech>
2024-02-02 15:56:10 +0100

    heya

--- jskeyboard.js
+++ jskeyboard.js
@@ -8,12 +8,33 @@ SwitchBtnsOff.forEach(btnoff => {
     });
 });
 
+
 const SwitchCap = document.getElementById('tgl-key-caps')
 const SwitchText = document.getElementById('tgl-key-text')
 
-SwitchText.addEventListener('click', ChangeTarget());
-SwitchCap.addEventListener('click', ChangeTarget());
+SwitchText.addEventListener('click', function(e) {ChangeTarget()});
+SwitchCap.addEventListener('click', function(e) {ChangeTarget()});
+
 function ChangeTarget(){
     SwitchText.classList.toggle('active');
     SwitchCap.classList.toggle('active');
-}
+};
+
+var btnOn = document.querySelector('.tgl-color.active')
+
+/*const Beckgroundtouchessss = document.querySelector('g:not(.Web_-_Click_Area) > *')
+const BackgroundtoucheS = document.querySelectorAll('#W-Click_Area path')
+console.log(BackgroundtoucheS)
+
+BackgroundtoucheS.forEach(Backgroundtouche => {
+    Backgroundtouche.addEventListener('click', function Changebackground(){
+        console.log("coucou");
+        Backgroundtouche.attributes.fill.nodeValue = (btnOn.attributes.datahex.nodeValue);
+    });
+});*/
+
+$(document).ready(function(){
+    $('g:not(.Web_-_Click_Area) > *').click(function() {
+        console.log(self)
+    });
+});
